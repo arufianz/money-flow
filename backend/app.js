2954f56f73db573222414b1823b95fdffddc4f7a
arufianz <alfianwashere@gmail.com>
2026-02-07 21:52:20 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
 
 
 app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -21,6 +22,6 @@ app.post("/add", (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Money App listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Money App listening on port ${PORT}`)
 })
